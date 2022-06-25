04093203e0db782c657426c87abd8deb23293c10
FCA606-00\teacher <t044@edu.fca.ac.jp>
2022-06-25 12:42:07 +0900

    プレハブ追加

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -8,20 +8,43 @@ public class BlockController : MonoBehaviour
     [SerializeField]
     private GameObject blockPrefab;
 
+    // 列の数
+    [SerializeField]
+    private int row = 3;
+
+    // 行の数
+    [SerializeField]
+    private int col = 3;
+
+    // 列間
+    [SerializeField]
+    private float rowSpace = 2f;
+
+    // 行間
+    [SerializeField]
+    private float lowSpace = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
+        // ブロックのサイズを考慮して列/行のスペースを決める
+        var blockScale = blockPrefab.transform.localScale;
+        blockScale.x += rowSpace;
+        blockScale.y += lowSpace;
+
         // 複数個ブロック作成
-        for (var i = 0; i < 3; i++)
+        for (var i = 0; i < col; i++)
         {
-            for (var j = 0; j < 3; j++)
+            for (var j = 0; j < row; j++)
             {
                 // グリッド上にプレハブ生成(BlockParentが親)
                 // transform：このスクリプトがアタッチされているオブジェクトのTransform
                 var block = Instantiate(blockPrefab, transform).transform;
 
                 // 座標を合わせる
-                block.localPosition = new Vector3(-5f + 5f * j, -2f + 2f * i, 0f);
+                block.localPosition = new Vector3(-blockScale.x + blockScale.x * j,
+                                                  -blockScale.y + blockScale.y * i,
+                                                  0f);
             }
         }
     }
